e89cab8611e1fdc6e7492197e7d09652e9cbb6e0
Dzaka Fadhlillah Hakim <dzaka04@gmail.com>
2024-03-03 09:25:41 +0700

    New Package

---
+++ Factory/Sortable.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Factory;
+
+/**
+ *
+ * @author dzaka
+ */
+interface Sortable{ // [TASK 3.1] Abstract class named Sortable.
+    int compare(Sortable b);
+    static void shell_sort(Sortable[] a){ 
+    //Shell sort body
+    }
+}
